beca6f2190b6bcf86c54c2e3a5442d2024447017
Jonas Abreu <jonas@vidageek.net>
2009-07-14 22:26:04 -0300

    updating junit to 4.6 (same runner as infinitest)

--- src/test/java/net/vidageek/mirror/provider/ReflectionProviderDataPointList.java
+++ src/test/java/net/vidageek/mirror/provider/ReflectionProviderDataPointList.java
@@ -2,6 +2,8 @@ package net.vidageek.mirror.provider;
 
 import net.vidageek.mirror.provider.java.PureJavaReflectionProvider;
 
+import org.junit.experimental.theories.DataPoint;
+
 /**
  * Interface to provide datapoints to JUnit's Theory.
  * 
@@ -9,6 +11,7 @@ import net.vidageek.mirror.provider.java.PureJavaReflectionProvider;
  */
 public interface ReflectionProviderDataPointList {
 
-	public ReflectionProvider pureJava = new PureJavaReflectionProvider();
+    @DataPoint
+    public ReflectionProvider pureJava = new PureJavaReflectionProvider();
 
 }
