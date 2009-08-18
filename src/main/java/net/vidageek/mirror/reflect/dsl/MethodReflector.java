013375c3304f141ce68be333dde2d248083bc6ed
Jonas Abreu <jonas@vidageek.net>
2009-08-18 14:14:52 -0300

    fixing small javadoc error (link was pointing to implementation class)

--- src/main/java/net/vidageek/mirror/reflect/dsl/MethodReflector.java
+++ src/main/java/net/vidageek/mirror/reflect/dsl/MethodReflector.java
@@ -3,24 +3,23 @@ package net.vidageek.mirror.reflect.dsl;
 import java.lang.reflect.Method;
 
 import net.vidageek.mirror.provider.ClassReflectionProvider;
-import net.vidageek.mirror.reflect.DefaultMethodReflector;
 
 public interface MethodReflector {
-	/**
-	 * This is a convenience method for
-	 * {@link DefaultMethodReflector#withArgs(Class...)}
-	 */
-	public Method withoutArgs() ;
+    /**
+     * This is a convenience method for
+     * {@link MethodReflector#withArgs(Class...)}
+     */
+    public Method withoutArgs();
 
-	/**
-	 * Use this method to reflect a method with classes as its argument types.
-	 * 
-	 * @param classes
-	 *            argument types of the method to be reflected.
-	 * @return A method matching classes as argument types.
-	 * @see ClassReflectionProvider#reflectMethod(String, Class[])
-	 * @throws IllegalArgumentException
-	 *             if classes is null
-	 */
-	public Method withArgs(final Class<?>... classes) ;
+    /**
+     * Use this method to reflect a method with classes as its argument types.
+     * 
+     * @param classes
+     *            argument types of the method to be reflected.
+     * @return A method matching classes as argument types.
+     * @see ClassReflectionProvider#reflectMethod(String, Class[])
+     * @throws IllegalArgumentException
+     *             if classes is null
+     */
+    public Method withArgs(final Class<?>... classes);
 }
