2372dce7111f3369a168d7b13be9f537621db74b
Jonas Abreu <jonas@vidageek.net>
2011-03-16 11:02:44 -0300

    fixed test suite

--- src/test/java/net/vidageek/mirror/provider/experimental/sun15/Sun15CompatibilityTest.java
+++ src/test/java/net/vidageek/mirror/provider/experimental/sun15/Sun15CompatibilityTest.java
@@ -3,14 +3,18 @@ package net.vidageek.mirror.provider.experimental.sun15;
 import junit.framework.Test;
 import net.vidageek.mirror.provider.test.ReflectionProviderTest;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
 /**
  * @author jonasabreu
  * 
  */
+@RunWith(Suite.class)
 final public class Sun15CompatibilityTest {
 
-    public static Test suite() {
-        return new ReflectionProviderTest(new Sun15ReflectionProvider());
-    }
+	public static Test suite() {
+		return new ReflectionProviderTest(new Sun15ReflectionProvider());
+	}
 
 }
