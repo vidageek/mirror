303175d7127acf889397cdcfae00fbc93b415585
Jonas Abreu <jonas@vidageek.net>
2011-03-19 14:20:28 -0300

    moving cglib dep to mirror (scope is provided)

--- test.sh
+++ test.sh
@@ -2,8 +2,6 @@
 
 echo "installing Mirror so we can test things after the jar is built"
 
-cd magic; mvn -Dmaven.test.skip clean install; cd ..;
-
 mvn -Dmaven.test.skip -Ptests clean install
 
 mvn test
