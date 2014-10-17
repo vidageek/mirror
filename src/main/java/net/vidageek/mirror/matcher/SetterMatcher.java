6173817c8a717adced574a41e4bfe22f458bc2c1
Otávio Garcia <otavio@otavio.com.br>
2014-10-17 11:22:56 -0300

    Setters may not be void. Closes #67

--- src/main/java/net/vidageek/mirror/matcher/SetterMatcher.java
+++ src/main/java/net/vidageek/mirror/matcher/SetterMatcher.java
@@ -1,5 +1,7 @@
 package net.vidageek.mirror.matcher;
 
+import static java.lang.reflect.Modifier.isAbstract;
+
 import java.lang.reflect.Method;
 
 import net.vidageek.mirror.list.dsl.Matcher;
@@ -7,8 +9,7 @@ import net.vidageek.mirror.list.dsl.Matcher;
 final public class SetterMatcher implements Matcher<Method> {
 
 	public boolean accepts(final Method element) {
-		return element.getName().startsWith("set") && (element.getParameterTypes().length == 1)
-				&& (element.getReturnType().equals(void.class));
+		return element.getName().startsWith("set") && element.getParameterTypes().length == 1
+				&& !element.isSynthetic() && !element.isBridge() && isAbstract(element.getModifiers());
 	}
-
 }
