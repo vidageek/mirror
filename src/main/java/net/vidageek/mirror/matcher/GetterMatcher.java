035b72bc58084b6a8c5116d5587a4416414bc188
Argan <argan.wang@gmail.com>
2015-06-11 09:43:05 +0800

    enhance GetterMatcher to match boolean getter 'isSomething'

--- src/main/java/net/vidageek/mirror/matcher/GetterMatcher.java
+++ src/main/java/net/vidageek/mirror/matcher/GetterMatcher.java
@@ -7,7 +7,8 @@ import net.vidageek.mirror.list.dsl.Matcher;
 final public class GetterMatcher implements Matcher<Method> {
 
 	public boolean accepts(final Method element) {
-		return element.getName().startsWith("get") && (element.getParameterTypes().length == 0)
-				&& (!element.getReturnType().equals(void.class));
+		return (element.getName().startsWith("get") || (element.getName().startsWith("is")
+				&& (element.getReturnType() == boolean.class || element.getReturnType() == Boolean.class)))
+				&& (element.getParameterTypes().length == 0) && (!element.getReturnType().equals(void.class));
 	}
 }
